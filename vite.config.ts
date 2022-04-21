0c99e414c96060131d84f559114428c1fe3d5b29
ONEO <oneo.me@outlook.com>
2022-04-21 17:48:50 +0800

    修复 Github Page 路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
     plugins: [svelte()],
+    base: "/svelte-fluentui-icons/",
     build: {
         outDir: 'docs'
     }
