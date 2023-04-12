9805f6ab2f799458983fcd59bafdbc3206ac3e03
ONEO <oneo.me@outlook.com>
2023-04-12 15:49:49 +0800

    优化样式

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
     import Github_Filled from "../icons/Github_Filled.svelte";
+    import Cube_Filled from "../icons/Cube_Filled.svelte";
     import Search_Filled from "../icons/Search_Filled.svelte";
+    import Earth_Filled from "../icons/Earth_Filled.svelte";
 
     import Icon from "./Icon.svelte";
     import Icons from "../Index.js";
     import { onMount } from "svelte";
 
-    document.body.className = "antialiased bg-white dark:bg-slate-900";
+    document.body.className =
+        "antialiased bg-white dark:bg-slate-900 text-slate-700 dark:text-slate-300";
 
     let source = [];
     let icons = [];
@@ -14,7 +17,7 @@
 
     function onInput() {
         let filterBy = keyword.toLowerCase();
-        if (keyword === "") icons = source;
+        if (filterBy === "") icons = source;
         else
             icons = source.filter((icon) =>
                 icon.key.toLowerCase().includes(filterBy)
@@ -25,97 +28,68 @@
         source = Icons;
         onInput();
     });
-
-    let copyKey = "";
-
-    function handleCopy(event) {
-        copyKey = event.detail;
-
-        console.log(copyKey);
-    }
 </script>
 
-<div
-    class="sticky top-0 z-40 w-full
-    backdrop-blur
-    bg-white/90 dark:bg-slate-900/75
-    border-b border-slate-900/10 dark:border-slate-50/[0.06]
-    mb-10"
+<a href="/"
+    ><h1 class="text-center text-3xl my-10">Svelte Fluent UI Icons</h1></a
 >
-    <div
-        class="container mx-auto p-4
-            text-lg
-            flex flex-row gap-3"
+<div class="flex flex-row justify-center space-x-6">
+    <a
+        class="hover:text-cyan-500 dark:hover:text-cyan-400 transition duration-100"
+        href="https://oneo.me"
+        target="_break"><Earth_Filled size="30" /></a
     >
-        <div class="basis-1/3 group relative">
-            <Search_Filled
-                size="20"
-                class="absolute left-3 top-1/2 -mt-2.5
-                    text-slate-400 pointer-events-none
-                    group-focus-within:text-cyan-500
-                    dark:group-focus-within:text-cyan-500"
-                aria-hidden="true"
-            />
-            <input
-                bind:value={keyword}
-                on:input={onInput}
-                class="pl-10 py-2
-                rounded-full
-                bg-slate-300/10
-                dark:bg-slate-600/10
-
-                text-slate-600 placeholder-slate-400
-                dark:text-white dark:placeholder-slate-100/50
-
-                ring-1 focus:outline-none
-
-                ring-slate-600 focus:ring-cyan-500
-                dark:ring-slate-200 dark:focus:ring-cyan-300
-
-                appearance-none text-sm"
-                type="text"
-                aria-label="Search icons"
-                placeholder="Search icons..."
-            />
-        </div>
-        <div class="basis-2/3 text-center">
-            <a
-                class="font-bold
-                underline underline-offset-4
-                text-slate-600 dark:text-white
-                hover:text-cyan-500 dark:hover:text-cyan-300"
-                href="/svelte-fluentui-icons">Svelte Fluent UI Icons</a
-            >
-        </div>
-        <div class="basis-1/3 grid justify-items-stretch items-stretch">
-            <a
-                href="https://github.com/oneo-me/svelte-fluentui-icons"
-                target="_break"
-                class="justify-self-end self-center
-                    text-slate-600 dark:text-white
-                    hover:text-cyan-500 dark:hover:text-cyan-300"
-                ><Github_Filled size="32" />
-            </a>
-        </div>
+    <a
+        class="hover:text-cyan-500 dark:hover:text-cyan-400 transition duration-100"
+        href="https://www.npmjs.com/package/svelte-fluentui-icons"
+        target="_break"><Cube_Filled size="30" /></a
+    >
+    <a
+        class="hover:text-cyan-500 dark:hover:text-cyan-400 transition duration-100"
+        href="https://github.com/oneo-me/svelte-fluentui-icons"
+        target="_break"><Github_Filled size="30" /></a
+    >
+</div>
+<div class="my-10 flex justify-center">
+    <div class="relative group">
+        <Search_Filled
+            size="22"
+            class="absolute top-1/2 -mt-2.5 left-2.5 text-slate-400
+             pointer-events-none
+        group-focus-within:text-cyan-500
+        transition duration-100
+    "
+        />
+        <input
+            type="text"
+            bind:value={keyword}
+            on:input={onInput}
+            class="appearance-none focus:outline-none
+            w-80 bg-slate-50 dark:bg-slate-800
+            border rounded-2xl pl-9 pr-3 py-2
+            border-slate-300 focus:border-cyan-500
+            dark:border-slate-500 dark:focus:border-cyan-400
+            transition duration-100
+        "
+            aria-label="Search icons"
+            placeholder="Search icons..."
+        />
     </div>
 </div>
 
 <div class="container mx-auto px-4">
-    <div class="flex flex-wrap justify-center gap-8 my-4">
+    <div class="flex flex-wrap justify-center gap-4 my-4">
         {#each icons as icon}
-            <Icon key={icon.key} value={icon.value} on:copy={handleCopy} />
+            <Icon key={icon.key} value={icon.value} />
         {/each}
     </div>
 </div>
 
 <div
-    class="mt-10 p-10 sticky
-    border-dashed border-t border-slate-900/10 dark:border-slate-50/[0.06]"
+    class="m-10 p-10
+    border-dashed border-t border-slate-900/10 dark:border-slate-50/20"
 >
-    <div
-        class="container mx-auto
-    text-slate-800 dark:text-slate-300 text-center"
-    >
+    <div class="container mx-auto text-center">
         <p>
             由
             <a
--- src/Icon.svelte
+++ src/Icon.svelte
@@ -1,28 +1,18 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
-
     export let key;
     export let value;
 
-    const dispatch = createEventDispatcher();
-
     function onClick() {
         navigator.clipboard.writeText(key);
-
-        dispatch("copy", key);
     }
 </script>
 
 <div
-    class="grid place-content-center p-2
-    rounded-lg
-    shadow-md hover:shadow-xl dark:shadow-slate-900
-    bg-white dark:bg-slate-500/30
-
-    ring-1 ring-slate-900/30 hover:ring-cyan-500/80 dark:ring-cyan-300/50 hover:ring-2 dark:hover:ring-cyan-300
-    text-slate-500 dark:text-white
-    transition duration-100 hover:scale-110 active:scale-105"
+    class="rounded-2xl p-3
+    bg-slate-500/5 hover:bg-slate-500/30 active:bg-slate-500/20
+    dark:bg-slate-200/5 dark:hover:bg-slate-200/30 dark:active:bg-slate-200/20
+     transition duration-100"
     on:click={onClick}
 >
-    <svelte:component this={value} size="55" />
+    <svelte:component this={value} size="45" />
 </div>
